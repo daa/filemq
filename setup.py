e39b03845ee0ae21831324ddc429fce45d7d63df
daa <adfrom2003@mail.ru>
2009-12-14 09:55:41 +0300

    readme changed

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+setup(name='python-filemq',
+	version='0.2',
+	description='File based message queue',
+	author='Alexander Duryagin',
+	author_email='daa@vologda.ru',
+	url='',
+	py_modules=['filemq'])
+
